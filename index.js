854f40002812666493d3ddaa27a2dbbd55343ce8
Joe Cianflone <joe@cianflone.co>
2016-11-04 22:57:55 -0400

    version bump, adds abilty to use sync and other options

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ var Elixir = require('laravel-elixir'),
 
 var Task = Elixir.Task;
 
-Elixir.extend('del', function (arr) {
+Elixir.extend('del', function (arr, options) {
+    options = (typeof options === "undefined") ? {} : options;
+
     new Task('del', function () {
-        return del(arr);
+        if (options.useSync) {
+            return del.sync(arr, options);
+        }
+
+        return del(arr, options);
     });
 });
